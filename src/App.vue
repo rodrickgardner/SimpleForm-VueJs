616acb5a28225290b67b5d21f2948096757d46ed
rodrick gardner opondo <rodrickgardner@gmail.com>
2020-10-01 18:27:50 +0300

    Commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+    <add-form></add-form>
+
+
+
+
+  </div>
+</template>
+
+<script>
+import addForm from './components/addForm.vue';
+
+export default {
+  components:{
+    'add-form':addForm
+  },
+  
+
+  data(){
+    return{
+
+
+    }
+  },
+  methods:{
+
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin:0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+#add-form*{
+  box-sizing:border-box;
+}
+#add-form{
+  margin:20px auto;
+  max-width:500px;
+}
+label{
+  display:block;
+  margin:20px 0 10px;
+}
+input[type="text"],textbox{
+  display:block;
+  width:100%;
+  align:center;
+  padding:8px;
+}
+#preview{
+  padding:10px 20px;
+  border:1px dotted #ccc;
+  margin:30px 0;
+}
+h3{
+  margin-top:10px;
+}
+</style>
---
+++ src/components/addForm.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="add-form">
+    <h2>Add a New Item</h2>
+    <form>
+    <label>Name</label>
+    <input type="text" v-model.lazy="name" required placeholder="Name">
+
+    <label>Quantity</label>
+    <!--<input type="text"v-model.lazy="quantity" required>-->
+    <input :value="quantity" @change="updateQuantity" type="number" class="form-control" name="quantity" id="quantity" placeholder="Quantity">
+
+
+
+    <label>Unit Price</label>
+    <!--<input type="text"v-model.lazy="price" required>-->
+    <input :value="price"  @change="updateRate" type="number" step="any" class="form-control" name="price" id="price" placeholder="Price">
+
+    <label>Total</label>
+    <!--<input type="text"v-model.lazy="total" required>-->
+
+    <input v-model="total" type="number" step="any" class="form-control" name="total" id="total" placeholder="Total Price">
+
+    </form>
+
+    <div id="preview">
+      <h3>Preview</h3>
+      <p>Name:{{name}}</p>
+      <p>Quantity:{{quantity}}</p>
+      <p>Unit Price:{{price}}</p>
+      <p>Total:{{total}}</p>
+    </div>
+    <button>Save</button>
+    <button type="reset">Clear</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+
+  data(){
+    return{
+    name:"",
+    quantity:"",
+    price:"",
+    total:""
+
+
+    }
+  },
+  methods:{
+
+    updateQuantity(event) {
+        this.quantity = event.target.value
+        this.total = this.quantity * this.price
+    },
+    updateRate(event) {
+        this.price = event.target.value
+        this.total = this.quantity * this.price
+    }
+
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
+ 
